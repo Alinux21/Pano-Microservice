af3a73de9f887093a271fa013bc998c969a082a7
Isache Bogdan <118884919+Bobala22@users.noreply.github.com>
2024-06-13 09:55:34 +0300

    Update panoController.js

--- Controllers/panoController.js
+++ Controllers/panoController.js
@@ -4,9 +4,9 @@ const PanoramaModel = require('../Models/panoModel.js');
 async function httpGetPanoramaScene(req, res) {
     
 
-     const hotel = req.url.split('/')[3].split('?')[1].split('&')[0].split('=')[1];
-     const appType = req.url.split('/')[3].split('?')[1].split('&')[1].split('=')[1];
-     const appId = req.url.split('/')[3].split('?')[1].split('&')[2].split('=')[1];
+     const hotel = req.url.split('/')[2].split('?')[1].split('&')[0].split('=')[1];
+     const appType = req.url.split('/')[2].split('?')[1].split('&')[1].split('=')[1];
+     const appId = req.url.split('/')[2].split('?')[1].split('&')[2].split('=')[1];
 
     try {
         console.log('getPanoramaScene');
